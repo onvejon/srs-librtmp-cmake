576ffe7f32a22fd0c1f9098ebb7e0b1592889f09
hwq@peergine.com <hwq@peergine.com>
2018-08-09 17:00:56 +0800

    build win32 lib success

---
+++ srs_win32def.h
@@ -0,0 +1,38 @@
+﻿
+#ifndef SRS_WIN32_DEF_H
+#define SRS_WIN32_DEF_H
+/*************************************************************
+**************************************************************
+* Windows SRS-LIBRTMP pre-declare
+**************************************************************
+*************************************************************/
+// for srs-librtmp, @see https://github.com/ossrs/srs/issues/213
+#ifdef _WIN32
+    // include windows first.
+	
+	#include <inttypes.h>
+	#include <WS2tcpip.h>
+	#include <ws2def.h>
+	#include <Windows.h>
+	#include <stdint.h>
+    // the type used by this header for windows.
+    typedef unsigned long long u_int64_t;
+    //typedef long long int64_t;
+    typedef unsigned int u_int32_t;
+    //typedef u_int32_t uint32_t;
+    //typedef int int32_t;
+    typedef unsigned char u_int8_t;
+	//typedef char int8_t;
+    typedef unsigned short u_int16_t;
+    //typedef short int16_t;
+    typedef int64_t ssize_t;
+
+    struct iovec {
+        void  *iov_base;    /* Starting address */
+        size_t iov_len;     /* Number of bytes to transfer */
+    };
+#endif
+
+#include <sys/types.h>
+
+#endif
---
+++ srs_win32def2.h
@@ -0,0 +1,63 @@
+﻿#ifndef SRS_WIN32_DEF_2_H
+#define SRS_WIN32_DEF_2_H
+
+/*************************************************************
+**************************************************************
+* Windows SRS-LIBRTMP solution
+**************************************************************
+*************************************************************/
+// for srs-librtmp, @see https://github.com/ossrs/srs/issues/213
+#ifdef _WIN32
+    #include "srs_win32def.h"
+    // for time.
+    #define _CRT_SECURE_NO_WARNINGS
+    #include <time.h>
+   
+    #define PRId64 "lld"
+    
+    // for inet helpers.
+    typedef int socklen_t;
+    //const char *inet_ntop(int af, const void *src, char *dst, socklen_t size);
+    
+    // for mkdir().
+    #include<direct.h>
+    
+    // for open().
+    typedef int mode_t;
+    #define S_IRUSR 0
+    #define S_IWUSR 0
+    #define S_IXUSR 0
+    #define S_IRGRP 0
+    #define S_IWGRP 0
+    #define S_IXGRP 0
+    #define S_IROTH 0
+    #define S_IXOTH 0
+    
+    // for file seek.
+    #include <io.h>
+    #include <fcntl.h>
+    
+#ifdef __cplusplus
+	extern "C" {
+#endif   
+		int gettimeofday(struct timeval* tv, struct timezone* tz);
+		// for pid.
+		typedef int pid_t;
+		pid_t getpid(void);
+
+		// for socket.
+		ssize_t writev(int fd, const struct iovec *iov, int iovcnt);
+		typedef int64_t useconds_t;
+		int usleep(useconds_t usec);
+		int socket_setup();
+		int socket_cleanup();
+#ifdef __cplusplus
+	}
+#endif       
+    // others.
+
+	//#define snprintf _snprintf
+    
+#endif
+
+#endif
